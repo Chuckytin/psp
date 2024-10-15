8b8fb8082ad7d1fdc59e8465d8cdb283c9dac091
Chuckytin <angeltorcalcatalan@gmail.com>
2024-10-15 20:17:26 +0200

    subiendo ejecuta comando timeout

---
+++ ejecutacomandotimeout/ejecutacomandotimeout/EjecutaComandoTimeOut.java
@@ -0,0 +1,58 @@
+package ejecutacomandotimeout;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+/*
+Ejemplo: 
+Piensa que hace (Linux)
+Cópialo
+Pruébalo
+¿Qué hace redirectStream?
+¿Qué pasa si no lo haces?
+
+ */
+public class EjecutaComandoTimeOut {
+
+	public static int MAX_TIEMPO = 500;
+	
+	public static void main(String[] args){
+		
+		ProcessBuilder pb = new ProcessBuilder(new String[]{"find", "/", "-name", "\"*\""});
+		
+		System.out.printf("Se ejecuta comando: %s\n", Arrays.toString(args));
+		
+		pb.inheritIO();
+		pb.redirectErrorStream(true);
+		
+		try{
+			
+			Process p = pb.start();
+			
+			if(!p.waitFor(MAX_TIEMPO, TimeUnit.MILLISECONDS)){
+				
+				p.destroy();
+				System.out.printf("AVISO: No ha terminado en %d ms\n", MAX_TIEMPO);
+				
+			}
+			
+		} catch (IOException e){
+			
+			System.err.println("Error durante la ejecucion del proceso");
+			System.err.println("Informacion detallada");
+			System.err.println("------------------");
+			e.printStackTrace();
+			System.err.println("------------------");
+			System.exit(1);
+			
+		} catch (InterruptedException e){
+			
+			System.err.println("Proceso interrumpido");
+			System.exit(2);
+			
+		}
+		
+	}
+	
+}
