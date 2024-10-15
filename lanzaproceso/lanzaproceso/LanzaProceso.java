4aa5dbb7b080e77434423c06ed5991d3b2a2cf27
Chuckytin <angeltorcalcatalan@gmail.com>
2024-10-15 19:09:06 +0200

    subiendo primer ejercicio psp

---
+++ lanzaproceso/lanzaproceso/LanzaProceso.java
@@ -0,0 +1,45 @@
+package lanzaproceso;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class LanzaProceso {
+
+	public static void main(String[] args){
+		
+		if(args.length<=0){
+			System.out.println("Debe indicarse comando a ejecutar.");
+			System.exit(1);
+		}
+		
+		ProcessBuilder pb = new ProcessBuilder(args);
+		pb.inheritIO();
+		
+		try{
+			
+			Process p = pb.start();
+			int codRet = p.waitFor();
+			System.out.println("La ejecucion de " 
+				+ Arrays.toString(args) + " devuelve " 
+				+ codRet + " " 
+				+ (codRet == 0? "(ejecucion correcta)" : "(ERROR)" ));
+			
+		} catch(IOException e){
+			
+			System.err.println("Error durante la ejecucion del proceso");
+			System.err.println("Informacion detallada");
+			System.err.println("------------------");
+			e.printStackTrace();
+			System.err.println("------------------");
+			System.exit(2);
+			
+		} catch(InterruptedException e){
+			
+			System.err.println("Proceso interrumpido");
+			System.exit(3);
+			
+		}
+		
+	}
+	
+}
